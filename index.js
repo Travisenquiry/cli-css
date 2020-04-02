2fc952eda3c1e15031955b453e99a32b88692ca4
travisenquiry <travisenquiry@gmail.com>
2020-04-02 18:27:46 +0800

    part 1 completed

---
+++ index.js
@@ -0,0 +1,20 @@
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+function hexToRgb(hex) {
+    var bigint = parseInt(hex, 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+
+    return r + "," + g + "," + b;
+}
+
+console.log("rgb( "+ hexToRgb(process.argv[3]) + " )"); // "51";
