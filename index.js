bfa04cb8e1f262298a2b2833a16b1a7d665e0771
travisenquiry <travisenquiry@gmail.com>
2020-04-02 19:21:21 +0800

    completed further part 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let rgbConverter = require("./rgbtohex.js");
 
 
 if(process.argv[2] === "hex"){
-  console.log("rgb( "+ hexConverter(process.argv[3]) + " )"); // "51";
+  console.log("rgb( "+ hexConverter(process.argv[3]) + " ), " + hexConverter.hexToHSL(process.argv[3])); // "51";
 }else if(process.argv[2] === "rgb"){
-  console.log(rgbConverter(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])));
+  console.log(rgbConverter(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])) + ", " +rgbConverter.rgbToHSL(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])));
 }
